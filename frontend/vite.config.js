dbcca8718cd221a31f76e8d6c546d05d1828fc73
Shruti799 <shrutijwal999@gmail.com>
2025-03-02 23:38:16 +0530

    changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1000, // Increase chunk size limit from 500kB to 1000kB
     rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Moves third-party dependencies into a separate chunk
+          }
+        },
+      },
       onwarn(warning, warn) {
         if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
           return; // Ignore the "use client" warnings
