344f9784502d060110d5d1d5241f0f4d858ce023
Shruti799 <shrutijwal999@gmail.com>
2025-03-02 21:51:25 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,13 @@ const commentRouter = require("./router/comments/commentRouter");
 connectDB();
 const app = express();
 //! PORT
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //Middlewares
 app.use(express.json()); //Pass json data
 // corse middleware
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
 };
 app.use(corse(corsOptions));
