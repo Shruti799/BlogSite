90a53a4d00c1e4fa6ca80dab3097c29f8d6ca460
Shruti799 <shrutijwal999@gmail.com>
2025-03-03 18:32:52 +0530

    changed

--- backend/controllers/users/userController.js
+++ backend/controllers/users/userController.js
@@ -1,178 +1,227 @@
-const asyncHandler = require("express-async-handler");
+// const asyncHandler = require("express-async-handler");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+// const passport = require("passport");
+// const User = require("../../models/User/User");
+
+// //-----User Controller---
+
+// const userController = {
+//   // !Register
+//   register: asyncHandler(async (req, res) => {
+//     const { username, email, password } = req.body;
+//     //Check if username already exist
+//     const userFound = await User.findOne({ username, email });
+//     if (userFound) {
+//       throw new Error("User already exists");
+//     }
+//     //Hash the password
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     //Register the user
+//     const userRegistered = await User.create({
+//       username,
+//       email,
+//       password: hashedPassword,
+//     });
+//     //send the response
+//     res.status(201).json({
+//       status: "success",
+//       message: "User registered successfully",
+//       userRegistered,
+//     });
+//   }),
+
+
+//   // ! Login
+//   login: asyncHandler(async (req, res, next) => {
+//     passport.authenticate("local", (err, user, info) => {
+//       if (err) return next(err);
+//       //check if user not found
+//       if (!user) {
+//         return res.status(401).json({ message: info.message });
+//       }
+//       //generate token
+//       const token = jwt.sign({ id: user?._id }, process.env.JWT_SECRET);
+//       //set the token into cookie
+//       res.cookie("token", token, {
+//         httpOnly: true,
+//         secure: true,
+//         sameSite: "None",
+//         maxAge: 24 * 60 * 60 * 1000, //1 day
+//       });
+
+//       //send the response
+//       res.json({
+//         status: "success",
+//         message: "Login Success",
+//         username: user?.username,
+//         email: user?.email,
+//         _id: user?._id,
+//       });
+//     })(req, res, next);
+//   }),
+
+
+//   // ! googleAuth-->
+//   googleAuth: passport.authenticate("google", { scope: ["profile"] }),
+//   // ! GoogleAuthCallback
+//   googleAuthCallback: asyncHandler(async (req, res, next) => {
+//     passport.authenticate(
+//       "google",
+//       {
+//         failureRedirect: "/login",
+//         session: false,
+//       },
+//       (err, user, info) => {
+//         if (err) return next(err);
+//         if (!user) {
+//           return res.redirect("https://bloggers-vdm1.onrender.com/google-login-error");
+//         }
+//         //generate the token
+
+//         const token = jwt.sign({ id: user?._id }, process.env.JWT_SECRET, {
+//           expiresIn: "3d",
+//         });
+//         //set the token into the cooke
+//         res.cookie("token", token, {
+//           httpOnly: true,
+//           secure: true,
+//           sameSite: "None",
+//           maxAge: 24 * 60 * 60 * 1000, //1 day:
+//         });
+//         //redirect the user dashboard
+//         res.redirect("https://bloggers-vdm1.onrender.com/dashboard");
+//       }
+//     )(req, res, next);
+//   }),
+  
+
+//   // ! check user authentication status
+//   checkAuthenticated: asyncHandler(async (req, res) => {
+//     const token = req.cookies["token"];
+//     if (!token) {
+//       return res.status(401).json({ isAuthenticated: false });
+//     }
+//     try {
+//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//       //find the user
+//       const user = await User.findById(decoded.id);
+//       if (!user) {
+//         return res.status(401).json({ isAuthenticated: false });
+//       } else {
+//         return res.status(200).json({
+//           isAuthenticated: true,
+//           _id: user?._id,
+//           username: user?.username,
+//           profilePicture: user?.profilePicture,
+//         });
+//       }
+//     } catch (error) {}
+//     return res.status(401).json({ isAuthenticated: false, error });
+//   }),
+  
+//    // ! Logout
+//    logout: asyncHandler(async (req, res) => {
+//     res.cookie("token", "", { maxAge: 1 });
+//     res.status(200).json({ message: "Logout success" });
+//   }),
+
+//   //! Profile
+//    profile: asyncHandler(async (req, res) => {
+//      const user = await User.findById(req.user)
+//        .populate("posts") // Keep only the posts
+//        .select("-password"); // Exclude sensitive fields
+   
+//      if (!user) {
+//        res.status(404);
+//        throw new Error("User not found");
+//      }
+   
+//      res.json({ user });
+//    }),
+
+// };
+
+
+// module.exports = userController;
+
+const asyncHandler = require("express-async-handler"); 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const User = require("../../models/User/User");
 
-//-----User Controller---
-
 const userController = {
-  // !Register
+
+  // Register
   register: asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
-    //Check if username already exist
     const userFound = await User.findOne({ username, email });
-    if (userFound) {
-      throw new Error("User already exists");
-    }
-    //Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-    //Register the user
-    const userRegistered = await User.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
-    //send the response
-    res.status(201).json({
-      status: "success",
-      message: "User registered successfully",
-      userRegistered,
-    });
-  }),
-  // ! Login
-  // login: asyncHandler(async (req, res, next) => {
-  //   passport.authenticate("local", (err, user, info) => {
-  //     if (err) return next(err);
-  //     //check if user not found
-  //     if (!user) {
-  //       return res.status(401).json({ message: info.message });
-  //     }
-  //     //generate token
-  //     const token = jwt.sign({ id: user?._id }, process.env.JWT_SECRET);
-  //     //set the token into cookie
-  //     res.cookie("token", token, {
-  //       httpOnly: true,
-  //       secure: true,
-  //       sameSite: "None",
-  //       maxAge: 24 * 60 * 60 * 1000, //1 day
-  //     });
-
-  //     //send the response
-  //     res.json({
-  //       status: "success",
-  //       message: "Login Success",
-  //       username: user?.username,
-  //       email: user?.email,
-  //       _id: user?._id,
-  //     });
-  //   })(req, res, next);
-  // }),
-
-  loginUser: asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+    if (userFound) throw new Error("User already exists");
     
-    // Find user
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userRegistered = await User.create({ username, email, password: hashedPassword });
+
+    res.status(201).json({ status: "success", message: "User registered successfully", userRegistered });
+  }),
+
+  // login
+  login: asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) throw new Error("User not found");
   
-    // Verify password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) throw new Error("Invalid credentials");
   
-    // Generate JWT token
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
   
-    // Send token in response (instead of cookie)
     res.json({ token, user });
   }),
 
+  // Google Auth
+  googleAuth: passport.authenticate("google", { scope: ["profile", "email"] }),
 
-  // ! googleAuth-->
-  // googleAuth: passport.authenticate("google", { scope: ["profile"] }),
-  // // ! GoogleAuthCallback
-  // googleAuthCallback: asyncHandler(async (req, res, next) => {
-  //   passport.authenticate(
-  //     "google",
-  //     {
-  //       failureRedirect: "/login",
-  //       session: false,
-  //     },
-  //     (err, user, info) => {
-  //       if (err) return next(err);
-  //       if (!user) {
-  //         return res.redirect("https://bloggers-vdm1.onrender.com/google-login-error");
-  //       }
-  //       //generate the token
-
-  //       const token = jwt.sign({ id: user?._id }, process.env.JWT_SECRET, {
-  //         expiresIn: "3d",
-  //       });
-  //       //set the token into the cooke
-  //       res.cookie("token", token, {
-  //         httpOnly: true,
-  //         secure: true,
-  //         sameSite: "None",
-  //         maxAge: 24 * 60 * 60 * 1000, //1 day:
-  //       });
-  //       //redirect the user dashboard
-  //       res.redirect("https://bloggers-vdm1.onrender.com/dashboard");
-  //     }
-  //   )(req, res, next);
-  // }),
-
+  // Google Auth Callback
   googleAuthCallback: asyncHandler(async (req, res, next) => {
     passport.authenticate("google", { session: false }, (err, user, info) => {
       if (err) return next(err);
-      if (!user) {
-        return res.redirect("https://bloggers-vdm1.onrender.com/google-login-error");
-      }
-      
-      // Generate JWT
+      if (!user) return res.redirect("https://bloggers-vdm1.onrender.com/google-login-error");
+
       const token = jwt.sign({ id: user?._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
-  
-      // Redirect with token in URL
+
       res.redirect(`https://bloggers-vdm1.onrender.com/dashboard?token=${token}`);
     })(req, res, next);
   }),
 
-  
-  
-  // ! check user authentication status
+  // Check Authentication Status
   checkAuthenticated: asyncHandler(async (req, res) => {
     const token = req.cookies["token"];
-    if (!token) {
-      return res.status(401).json({ isAuthenticated: false });
-    }
+    if (!token) return res.status(401).json({ isAuthenticated: false });
+
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      //find the user
       const user = await User.findById(decoded.id);
-      if (!user) {
-        return res.status(401).json({ isAuthenticated: false });
-      } else {
-        return res.status(200).json({
-          isAuthenticated: true,
-          _id: user?._id,
-          username: user?.username,
-          profilePicture: user?.profilePicture,
-        });
-      }
-    } catch (error) {}
-    return res.status(401).json({ isAuthenticated: false, error });
+      if (!user) return res.status(401).json({ isAuthenticated: false });
+
+      res.status(200).json({ isAuthenticated: true, _id: user?._id, username: user?.username, profilePicture: user?.profilePicture });
+    } catch (error) {
+      return res.status(401).json({ isAuthenticated: false, error });
+    }
   }),
-  
-   // ! Logout
-   logout: asyncHandler(async (req, res) => {
+
+  // Logout
+  logout: asyncHandler(async (req, res) => {
     res.cookie("token", "", { maxAge: 1 });
     res.status(200).json({ message: "Logout success" });
   }),
 
-  //! Profile
-   profile: asyncHandler(async (req, res) => {
-     const user = await User.findById(req.user)
-       .populate("posts") // Keep only the posts
-       .select("-password"); // Exclude sensitive fields
-   
-     if (!user) {
-       res.status(404);
-       throw new Error("User not found");
-     }
-   
-     res.json({ user });
-   }),
-
+  // Profile
+  profile: asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user).populate("posts").select("-password");
+    if (!user) throw new Error("User not found");
+    res.json({ user });
+  }),
 };
-
 
 module.exports = userController;
